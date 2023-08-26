fa7e962624571e80cdf30145a8cfc73c82bd9342
Michael <moreno.m2698@gmail.com>
2023-08-26 11:09:35 -0400

    Pushed tracks to be in the accordion contents

--- src/components/MusicSideBarComponents/AlbumAccordion.tsx
+++ src/components/MusicSideBarComponents/AlbumAccordion.tsx
@@ -28,34 +28,40 @@ function AlbumAccordion(props: AlbumAccordionProps) {
                 <div>
                     {albumTracks.map((track, index) => (
                         <div className='item'>
-                        <div className='title' onClick={() => props.accordionToggle(index)}>
-                            <h1>{track.title}</h1>
-                            <span>{props.selected === index ? '-' : '+' }</span>
-                        </div>
-                        <div className={props.selected === index? 'content show' : 'content'}>
-                            {track.artist}
-                        </div>
-                    </div>
+                            <div className='title' onClick={() => props.accordionToggle(index)}>
+                            <div className='music-tile-image'>
+                                <img src={props.album.image} alt={props.album.title}/>
+                            </div>
+                            <div className="track-info-container">
+                                
+                                <div className="album-tile-title">
+                                    <a role="button" title={props.album.title}>
+                                        <span>
+                                            {props.album.title}
+                                        </span>
+                                    </a>
+                                </div>
+                                <div className="album-tile-author">
+                                    <span>{props.album.artist}</span>
+                                </div>
+                            </div>
+                                <h1>{track.title}</h1>
+                                <span>{props.selected === index ? '-' : '+' }</span>
+                            </div>
 
+                           {/* This will be what extends out of the accordion */} 
+                            <div className={props.selected === index? 'content show' : 'content'}>
+                                {tracks.map((song) => (
+                                    <div>
+                                        {song.title}
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
                     ))}
                 </div>
             </div>
-                <div className='music-tile-image'>
-                    <img src={props.album.image} alt={props.album.title}/>
-                </div>
-                <div className="track-info-container">
-                    
-                    <div className="album-tile-title">
-                        <a role="button" title={props.album.title}>
-                            <span>
-                                {props.album.title}
-                            </span>
-                        </a>
-                    </div>
-                    <div className="album-tile-author">
-                        <span>{props.album.artist}</span>
-                    </div>
-                </div>
+                
 
         </>
     )
