b572255868d38843b990be2f5d22a985c248472b
Michael <moreno.m2698@gmail.com>
2023-08-29 15:41:21 -0400

    added section classnames for accordion divs

--- src/components/MusicSideBarComponents/AlbumUnfoldComponent.tsx
+++ src/components/MusicSideBarComponents/AlbumUnfoldComponent.tsx
@@ -12,21 +12,19 @@ interface AlbumUnfoldProps {
 //todo : put number next to each element
 //Also put an indicator to show which song is playing
 
-
-
-
-
 function AlbumUnfoldComponent(props: AlbumUnfoldProps) {
 
   const {currentTrackIndex, setCurrentTrackIndex, playerTracks, setPlayerTracks} = useContext(AppContext);
-  const trackSelect = (index) => {
+  const trackSelect = (index:number) => {
     if (playerTracks.id !== props.queueId) {
       const albumQueue = {
         id: props.queueId,
         tracks: props.tracks
       }
       
-      setPlayerTracks(albumQueue)
+      if (setPlayerTracks !== undefined) {
+        setPlayerTracks(albumQueue)
+      }
     }
 
     if (setCurrentTrackIndex !== undefined) {
@@ -37,10 +35,19 @@ function AlbumUnfoldComponent(props: AlbumUnfoldProps) {
   return (
     <div className={props.selectedIndex === props.albumIndex ? 'content show' : 'content'}>
         {props.tracks.map((song, index) => (
-            <div onClick={() => {trackSelect(index); console.log(playerTracks)}} key={index}>
-                {song.index + 1}
-                {song.title}
-                {song.artist}
+            <div onClick={() => {trackSelect(index); console.log(playerTracks)}} key={index} className='album-accordion-tracks'>
+              <div className='album-accordion-index'>
+                <span>{song.index + 1}</span>
+              </div>
+              <div className='album-accordion-track'>
+                <span>
+                  {song.title}
+                </span>
+                <span>
+                  {song.artist}
+                </span>
+              </div>
+              
             </div>
         ))}
     </div>           
